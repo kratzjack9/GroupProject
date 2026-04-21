73cda900adaa6cc450474562645b22f8c14db051
Eein McKinley <ewm005@morningside.edu>
2026-04-21 11:48:46 -0500

    Adding documentation on biome.py

--- src/biome.py
+++ src/biome.py
@@ -1,35 +1,114 @@
 """
-biome.py
-=========
-Implements a "game of life" simulated biome
+life.py
+====================================
+Game of life project with custom rules
 
-| Collin, Eein, Kade, Tobias
-| 2026 April 20
+| Author: Collin, Eein, Kade, Tobias
+| Date: 2026 April 20
 """
 
 from random import choices, random
 
 
 class Organism:
+    """
+    Creates Organism that has randomly created DNA
+
+    Parameters
+    -----------
+    energy : int
+        The amount of energy the organism starts with. Default is 100.
+
+    Attributes
+    ------------
+    dna : str
+        The organism's dna. 
+        Is three characters long
+
+    energy : int
+        The amount of energy that organism has at that moment.
+
+    isAlive : boolean
+        Tells if the organism is alive
+    """
     dnaOptions = ['🌱','💧','🌞','🎄','🍊']
 
     def __init__(self, energy=100):
+        """
+        Initialize a new Organism instance.
+
+        Parameters
+        -----------
+        energy : int
+            The amount of energy the organism starts with. Default is 100.
+
+        """
         # print("Organism initialized")
         self.dna = choices(Organism.dnaOptions, k=3)
         self.energy = energy
         self.isAlive = True
     
     def mutate(self):
+        """
+        This allows the Organism to change its attribute, dna
+        """
         pass
 
     def consume(self, other):
+        """
+        This allows an Organism to eat another Organism to gain energy
+        """
         pass
 
     def __str__(self):
+        """
+        The string returned when the Organism is a method that requires a string
+        """
         return ",".join(self.dna) + str(self.energy)
 
 class Biome:
+    """
+    Creates a Biome that holds all of the organisms
+
+    Parameters
+    ------------
+    nRows : int
+        The amount of rows in the biome grid
+
+    nCols : int
+        The amount of columns in the biome grid
+
+    startEnergy : int
+        The amount of start energy that Organisms have
+
+    Attributes
+    ------------
+    rows : int 
+        The amount of rows in the grid
+
+    cols : int
+        The amount of cols in the grid
+
+    grid : 2D list
+        The grid which holds Organisms
+
+    """
     def __init__(self, nRows=5, nCols=3, startEnergy=100):
+        """
+        Initialize a new Biome instance
+        
+        Parameters
+        ------------
+        nRows : int
+            The amount of rows in the biome grid
+
+        nCols : int
+            The amount of columns in the biome grid
+
+        startEnergy : int
+            The amount of start energy that Organisms have
+
+        """
         self.rows = nRows
         self.cols = nCols
         #List Comprehension Grid
@@ -38,7 +117,17 @@ class Biome:
         # print("Biome initialized")
     
     def hydration(self, row, column):
-        """Implements the hydration rule at location row,column"""
+        """
+        Implements the hydration rule at location row,column
+        
+        Parameters
+        ------------
+        row : int 
+            The row in which the hydration rule is used
+
+        column: int
+            The column in which the hydration rule is used
+        """
         if '💧' in self.grid[row][column].dna:
             print(f'[{row},{column}] has water')
             if row > 0:  # can check above
@@ -53,6 +142,9 @@ class Biome:
                 pass
     
     def step(self):
+        """
+        Implementation of all rules and reducing the energy of all Organisms
+        """
         self.cycleCount += 1  # increments count
         print('='*25)
         print(f"Currently day {self.cycleCount}")
@@ -72,6 +164,9 @@ class Biome:
         self.display()
     
     def display(self):
+        """
+        Displays the grid with all of the Organisms in their respective spots
+        """
         # print("Displaying the biome")
         for ii in range(len(self.grid)):
             for jj in range(len(self.grid[ii])):
@@ -80,6 +175,9 @@ class Biome:
 
 
 def main():
+    """
+    The primary running of the code
+    """
     # o1 = Organism()
     # print(o1)
     myBiome = Biome(nRows=5, nCols=3, startEnergy=100)
