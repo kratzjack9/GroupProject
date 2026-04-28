23acafe1c169db2562728e4805f2259e870ef184
cph005 <cph005@morningside.edu>
2026-04-28 11:53:55 -0500

    added space

--- src/biome.py
+++ src/biome.py
@@ -34,7 +34,9 @@ class Organism:
 
     isNewOrganism : boolean
         Tells if the organism was just born to prevent rules applying immediately
+        
     """
+
     dnaOptions = ['🌱','💧','🌞','🎄','🍊']
     dnaCombust = ["🌱🌱🌱","💧💧💧","🌞🌞🌞","🎄🎄🎄","🍊🍊🍊"]
 
